3ae30b35b1a22a39e370d4fdd89aadc1da124c7e
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-09-10 13:56:25 +0300

    create tracer project

---
+++ TracerTests/TracerTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TracerTests
+{
+    [TestClass]
+    public class TracerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
