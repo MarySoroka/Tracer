442a15380537c5050963e739ab1904710400788a
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-09-21 18:05:35 +0300

    Create tracer and tracer tests

--- TracerTests/TracerTest.cs
+++ TracerTests/TracerTest.cs
@@ -1,14 +1,34 @@
+using System.Collections.Concurrent;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TracerLibrary;
 
 namespace TracerTests
 {
     [TestClass]
     public class TracerTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private ITracer _tracer = new Tracer(new TracerResult(new ConcurrentDictionary<int, TracerThread>()));
+        public void TracerTestMethod()
         {
+            _tracer.StartTrace();
+            Thread.Sleep(100);
+            _tracer.StopTrace();
             
+        }
+        public void TracerTestInnerMethod()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(300);
+            _tracer.StopTrace();
+            
+        }
+        [TestMethod]
+        public void IsTracerRight()
+        {
+            TracerTestMethod();
+            var tracerResult = _tracer.GetTracerResult();
+            Assert.AreEqual(1, tracerResult.ConcurrentThead.Count);
         }
     }
 }
