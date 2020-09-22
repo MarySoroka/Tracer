96dead770e1836d442e3b7643a3c547ea53d2221
MarySoroka <m.soroka.01.06.01@gmail.com>
2020-09-22 12:32:59 +0300

    Add new tests and resolve method count problem

--- TracerTests/TracerTest.cs
+++ TracerTests/TracerTest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using System.Linq;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TracerLibrary;
@@ -8,27 +9,57 @@ namespace TracerTests
     [TestClass]
     public class TracerTest
     {
-        private ITracer _tracer = new Tracer(new TracerResult(new ConcurrentDictionary<int, TracerThread>()));
-        public void TracerTestMethod()
+        private readonly ITracer _tracer = new Tracer(new TracerResult(new ConcurrentDictionary<int, TracerThread>()));
+
+        private void TracerMethodTestOfTheSameLevel()
+        {
+            TracerTestMethod();
+            TracerTestMethod();
+        }
+
+        private void TracerTestMethod()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(100);
+            TracerTestInnerMethod();
+            _tracer.StopTrace();
+            
+        }
+
+        private void TracerTestInnerMethod()
         {
             _tracer.StartTrace();
             Thread.Sleep(100);
             _tracer.StopTrace();
             
         }
-        public void TracerTestInnerMethod()
+        [TestMethod]
+        public void AmountOfTraceMethods_IsRight()
         {
-            _tracer.StartTrace();
-            Thread.Sleep(300);
-            _tracer.StopTrace();
-            
+            TracerMethodTestOfTheSameLevel();
+            var tracerResult = _tracer.GetTracerResult();
+            Assert.AreEqual(2, tracerResult.ConcurrentThead.Values.ToArray()[0].ThreadMethods.Count);
         }
         [TestMethod]
-        public void IsTracerRight()
+        public void TracerTimeExecute_IsRight()
         {
             TracerTestMethod();
             var tracerResult = _tracer.GetTracerResult();
-            Assert.AreEqual(1, tracerResult.ConcurrentThead.Count);
+            Assert.AreEqual("200", tracerResult.ConcurrentThead.ToArray()[0].Value.ThreadTime);
         }
+        [TestMethod]
+        public void AmountOfTracerThreads_IsRight()
+        {
+            _tracer.StartTrace();
+            for (var i = 0; i < 6; ++i)
+            {
+                var thread = new Thread(TracerTestMethod);
+                thread.Start();
+            }
+            _tracer.StopTrace();
+            var actualCountOfThreads = _tracer.GetTracerResult().ConcurrentThead.Count;
+            Assert.AreEqual(6, actualCountOfThreads);
+        }
+
     }
 }
